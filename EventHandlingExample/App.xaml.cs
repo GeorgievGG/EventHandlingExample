06c6cfb63d9166a2c857848a31e90c1ff9c60239
Georgi Georgiev <ggfinz@gmail.com>
2021-03-25 14:16:56 +0200

    Remove unneeded todos.

--- EventHandlingExample/App.xaml.cs
+++ EventHandlingExample/App.xaml.cs
@@ -93,7 +93,6 @@ namespace EventHandlingExample
         private void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
             deferral.Complete();
         }
     }
