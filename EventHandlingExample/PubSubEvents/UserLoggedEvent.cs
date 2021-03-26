04e366e066b47b4ce046195769dfcb855c2f4a3e
Georgi Georgiev <ggfinz@gmail.com>
2021-03-26 15:30:01 +0200

    Include UserLoggedEvent in the repo.

---
+++ EventHandlingExample/PubSubEvents/UserLoggedEvent.cs
@@ -0,0 +1,9 @@
+﻿using AuthControl.Models;
+using EventHandlingExample.Common;
+
+namespace EventHandlingExample.PubSubEvents
+{
+    public class UserLoggedEvent : PubSubEvent<LoginInfo>
+    {
+    }
+}
